41a8651daf8440f8ca4917004de615241bc0f058
Ozan Çiçek <ozancck@icloud.com>
2024-12-25 11:26:00 +0300

    Initial commit

---
+++ E-CommerceApp-SwiftUI/E_CommerceApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceApp_SwiftUIApp.swift
+//  E-CommerceApp-SwiftUI
+//
+//  Created by Ozan Çiçek on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ E-CommerceApp-SwiftUITests/E_CommerceApp_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceApp_SwiftUITests.swift
+//  E-CommerceApp-SwiftUITests
+//
+//  Created by Ozan Çiçek on 25.12.2024.
+//
+
+import Testing
+@testable import E_CommerceApp_SwiftUI
+
+struct E_CommerceApp_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
